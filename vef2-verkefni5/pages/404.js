98c431d94d9bf0db57d5c69eb20a84d08bbb3688
Stina13 <89361289+Stina13@users.noreply.github.com>
2022-03-31 16:42:56 +0000

    setja upp 404

---
+++ vef2-verkefni5/pages/404.js
@@ -0,0 +1,8 @@
+//module.exports = (req, res) => {
+  //  res.status(404).json({ message: 'Not Found' });
+  //};
+
+  // pages/404.js
+export default function Custom404() {
+    return <h1>404 - Page Not Found</h1>
+  }
